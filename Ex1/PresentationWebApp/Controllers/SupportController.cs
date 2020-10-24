32d9542853a114cda2a437bc6fb7175dd2986e96
Grimm33 <yandrick.alpha.grima@gmail.com>
2020-10-24 10:18:46 +0200

    Initial Commit - contact us action

---
+++ Ex1/PresentationWebApp/Controllers/SupportController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationWebApp.Controllers
+{
+    public class SupportController : Controller
+    {
+        [HttpGet]
+        public IActionResult Contact()      //this will load the page
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(string email, string query)      //this will handle the form submission
+        {
+            //inform the responsable staff
+
+            ViewData["feedback"] = "Thank you for gettign in touch with us. We will answer back asap.";
+
+            return View();
+        }
+    }
+}
